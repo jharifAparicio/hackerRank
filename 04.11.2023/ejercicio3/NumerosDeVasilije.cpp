72a1b60ddd2a4348cd448323e0086f1b608f3ab9
Jharif Nelson Aparicio Casillas <jharif.aparicio.casillas@usfx.bo>
2024-05-25 13:55:56 -0400

    feat: Implement Vasilije's number finding algorithm
    The code changes include implementing Vasilije's number finding algorithm in the `find_numbers` function. This algorithm uses a combination of numbers to find a specific sum. This implementation allows for the solution of exercise 3.

--- 04.11.2023/ejercicio3/NumerosDeVasilije.cpp
+++ 04.11.2023/ejercicio3/NumerosDeVasilije.cpp
@@ -1,18 +1,58 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+
+using namespace std;
+
+void recibirNumeros(int &a, int &b, int &c) {
+    std::cin >> a >> b >> c;
+}
+
+vector<int> find_numbers(int n, int x, int m) {
+    vector<int> nums(x);
+    iota(nums.begin(), nums.end(), 1); // Rellenar nums con [1, 2, ..., x]
+    
+    queue<pair<vector<int>, int>> q;
+    q.push({{}, 0});
+
+    while (!q.empty()) {
+        auto [comb, start] = q.front();
+        q.pop();
+
+        if (comb.size() == n) {
+            if (accumulate(comb.begin(), comb.end(), 0) == m) {
+                return comb;
+            }
+            continue;
+        }
+
+        for (int i = start; i < nums.size(); ++i) {
+            if (accumulate(comb.begin(), comb.end(), 0) + nums[i] > m) {
+                break; // Poda: no considerar números que superen la suma deseada
+            }
+
+            vector<int> new_comb = comb;
+            new_comb.push_back(nums[i]);
+            q.push({new_comb, i + 1});
+        }
+    }
+
+    return {};
+}
+
 int main() {
-    long long int t;
-    std::cin >> t;
-    while (t--) {
-        long long int n, k, x,suma;
-        std::cin >> n >> k >> x;
-        suma = n*(n+1)/2;
-        
-        if((suma < x) ||
-        (k == x) || 
-        (n == 1)){
-            std::cout << "NO" << std::endl;
-        }else{
-            std::cout << "YES" << std::endl;
+    int numcase;
+    std::cin >> numcase;
+    int num1, num2, num3;
+    for (int i = 0; i < numcase; ++i) {
+        recibirNumeros(num1, num2, num3);
+        vector<int> result = find_numbers(num1, num2, num3);
+        if (!result.empty()) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
         }
     }
     return 0;
